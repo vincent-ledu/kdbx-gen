144385ee95a169c24089fa6be6dd0f3a1b1e0e28
Vincent Le Dû <vincent.ledu@gmail.com>
2022-04-03 22:17:20 +0200

    in progress

--- src/KdbxManager.ts
+++ src/KdbxManager.ts
@@ -14,14 +14,14 @@ function searchGroupByName(db: kdbxweb.Kdbx, groupName: string) {
 export async function createKDBX(jsonObj: any[], password: kdbxweb.ProtectedValue, name: string ) {
   kdbxweb.CryptoEngine.setArgon2Impl(argon2.argon2);
   const credentials = new kdbxweb.KdbxCredentials(password);
-  let db = kdbxweb.Kdbx.create(credentials, name);
+  const db = kdbxweb.Kdbx.create(credentials, name);
   jsonObj.forEach((row) => {
-    let groupKeys = Object.keys(row).filter(key => ['GROUP', 'GROUPE', 'GRP', 'ADABO'].includes(key.toUpperCase()));
+    const groupKeys = Object.keys(row).filter(key => ['GROUP', 'GROUPE', 'GRP', 'ADABO'].includes(key.toUpperCase()));
     let group = db.getDefaultGroup();
     let entry
     if (groupKeys.length > 0) {
       for (let i = 0; i < groupKeys.length; i += 1) {
-        let groupFound = searchGroupByName(db, row[groupKeys[i]])
+        const groupFound = searchGroupByName(db, row[groupKeys[i]])
         if (!groupFound) {
           group = db.createGroup(group, row[groupKeys[i]]);
         } else {
@@ -34,7 +34,7 @@ export async function createKDBX(jsonObj: any[], password: kdbxweb.ProtectedValu
     } else {
       entry = db.createEntry(group);
     }
-    for (let [key, value] of Object.entries(row)) {
+    for (const [key, value] of Object.entries(row)) {
       if (['PASSWORD', 'MOT DE PASSE', 'PASSE', 'PASS', 'PWD', 'MDP'].includes(key.toUpperCase())) {
         entry.fields.set('Password', kdbxweb.ProtectedValue.fromString(String(value)))
       } else if (['GROUP', 'GROUPE', 'GRP', 'ADABO'].includes(key.toUpperCase())) {
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,64 @@
-import express from 'express'
+import express from "express";
+import Logger from "./utils/logger";
+import "dotenv/config";
+import * as KdbxManager from "./KdbxManager";
+import { ProtectedValue } from "kdbxweb";
+import path from "path";
+import { I18n } from "i18n";
+import csv from "csvtojson";
 
+const i18n = new I18n();
+i18n.configure({
+  locales: ["en", "fr"],
+  directory: path.join(__dirname, "locales"),
+});
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.post('/kdbx', (req, res) => {
-  req.body.q
+app.post("/kdbx", (req, res) => {
+  Logger.info(req.body);
+  if (req.body.json) {
+    KdbxManager.createKDBX(
+      req.body.json,
+      ProtectedValue.fromString("test"),
+      "MyDB"
+    )
+      .then((db) => {
+        res.status(201).send(db.save());
+      })
+      .catch((err) => {
+        Logger.error(err);
+        res.status(500).send("Error while creating the stuff... sorry...");
+      });
+  } else {
+    csv()
+      .fromString(req.body.csv)
+      .then((jsonObj) => {
+        KdbxManager.createKDBX(
+          jsonObj,
+          ProtectedValue.fromString("test"),
+          "MyDb"
+        )
+          .then((db) => {
+            res.status(201).send(db.save());
+          })
+          .catch((err) => {
+            Logger.error(err);
+            res.status(500).send("Error while creating the stuff... sorry...");
+          });
+      });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("pages/createKeepass");
+});
+
+app.listen(port, () => {
+  Logger.info(`Listening server on port ${port}`);
 });
